0567d75fd43d2b3b2c4045248797c3292231cb59
Miguel DeBarros <migueldebarros34@gmail.com>
2023-03-12 19:41:06 -0400

    I finally figured it out.

---
+++ scripts.js
@@ -0,0 +1,31 @@
+        const spinningDiscoGif = 'https://raw.githubusercontent.com/micodes123/rickroll/main/assets/spinningDiscoGif.webp'
+
+        const brokenDisco = 'https://raw.githubusercontent.com/micodes123/rickroll/main/assets/image.gif';
+
+        const image = document.getElementById('image');
+
+        const turnOnButton =  document.getElementById("turn-on");
+
+        const turnOffButton = document.getElementById("turn-off");
+
+        const music = new Audio ('https://raw.githubusercontent.com/micodes123/rickroll/main/assets/music.mp3');
+
+        
+
+
+        turnOnButton
+        .addEventListener("click",
+             function(){ 
+              image.setAttribute("src", spinningDiscoGif);
+              music.play();
+              document.body.classList.add("rick-rolled")
+             });
+
+        turnOffButton
+        .addEventListener("click",
+             function(){ 
+              image.setAttribute("src",
+               brokenDisco);
+               music.pause();
+               document.body.classList.remove("rick-rolled")
+             });
